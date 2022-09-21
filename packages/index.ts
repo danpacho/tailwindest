84476fe928f8cc566d3f9e7a7bba1f18d5561376
Danpa_cho <danpa725@cau.ac.kr>
2022-09-21 22:53:12 +0900

    Setup: env with vitejs .

---
+++ packages/index.ts
@@ -0,0 +1,3 @@
+const init = "HELLOW WORLD!";
+
+export { init };
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+
+import dts from "vite-plugin-dts";
+
+import { resolve } from "path";
+
+export default defineConfig({
+    plugins: [dts()],
+    build: {
+        lib: {
+            entry: resolve(__dirname, "packages/index.ts"),
+            name: "tailwindest",
+            formats: ["es", "umd"],
+            fileName: (format: string) => `tailwindest.${format}.js`,
+        },
+    },
+});
