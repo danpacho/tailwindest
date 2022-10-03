1155265584a12530dd439bc1f1319bdfc824a88e
Danpa_cho <danpa725@cau.ac.kr>
2022-10-03 16:57:05 +0900

    :sparkles: Add: `flatten.object` util function

---
+++ packages/utils/flatten.object.ts
@@ -0,0 +1,16 @@
+import type { NestedObject } from "./nested.object.type"
+
+/**
+ * @param object string key object
+ * @returns Flatten nested object into `string[]`
+ */
+const flattenObject = <T extends NestedObject>(object: T): string[] =>
+    Object.values(object)
+        .map((value) =>
+            typeof value !== "string"
+                ? flattenObject(value as NestedObject)
+                : value
+        )
+        .flat()
+
+export { flattenObject }
