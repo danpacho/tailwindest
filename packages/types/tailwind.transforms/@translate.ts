ba872d041222b2a4d0c0db98d0c5e53bc0256060
Danpa_cho <danpa725@cau.ac.kr>
2022-10-05 22:20:11 +0900

    :label: Update: `tailwind.transforms` add opposite direction of `@translate` property

--- packages/types/tailwind.transforms/@translate.ts
+++ packages/types/tailwind.transforms/@translate.ts
@@ -17,17 +17,23 @@ export type TailwindTranslateType = {
      *@unit Gap `1` = `4px` = `0.25rem`
      *@docs [translate](https://tailwindcss.com/docs/translate)
      */
-    transformTranslate: `translate-${TailwindTranslateVariants}`
+    transformTranslate:
+        | `translate-${TailwindTranslateVariants}`
+        | `-translate-${TailwindTranslateVariants}`
     /**
      *@note Utilities for translating elements with transform x direction.
      *@unit Gap `1` = `4px` = `0.25rem`
      *@docs [translate](https://tailwindcss.com/docs/translate)
      */
-    transformTranslateX: `translate-x-${TailwindTranslateVariants}`
+    transformTranslateX:
+        | `translate-x-${TailwindTranslateVariants}`
+        | `-translate-x-${TailwindTranslateVariants}`
     /**
      *@note Utilities for translating elements with transform y direction.
      *@unit Gap `1` = `4px` = `0.25rem`
      *@docs [translate](https://tailwindcss.com/docs/translate)
      */
-    transformTranslateY: `translate-y-${TailwindTranslateVariants}`
+    transformTranslateY:
+        | `translate-y-${TailwindTranslateVariants}`
+        | `-translate-y-${TailwindTranslateVariants}`
 }
