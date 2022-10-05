eb46fd31685019165ac367d7c546b06b9bd5b8b4
Danpa_cho <danpa725@cau.ac.kr>
2022-10-05 20:02:13 +0900

    :label: Update: `tailwind.transform` add opposite direction of `@skew` property

--- packages/types/tailwind.transforms/@skew.ts
+++ packages/types/tailwind.transforms/@skew.ts
@@ -4,11 +4,15 @@ export type TailwindSkewType = {
      *@note Utilities for skewing elements with transform x direction
      *@docs [skew](https://tailwindcss.com/docs/skew)
      */
-    transformSkewX: `skew-x-${TailwindSkewVariants}`
+    transformSkewX:
+        | `skew-x-${TailwindSkewVariants}`
+        | `-skew-x-${TailwindSkewVariants}`
     /**
      *@note Utilities for skewing elements with transform y direction
      *@unit Gap `1` = `1deg`
      *@docs [skew](https://tailwindcss.com/docs/skew)
      */
-    transformSkewY: `skew-y-${TailwindSkewVariants}`
+    transformSkewY:
+        | `skew-y-${TailwindSkewVariants}`
+        | `-skew-y-${TailwindSkewVariants}`
 }
