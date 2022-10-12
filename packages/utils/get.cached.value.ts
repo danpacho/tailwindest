3d214fdce2abd8c69906c38c7eeedbb2af69d587
Danpa_cho <danpa725@cau.ac.kr>
2022-10-12 22:45:33 +0900

    :sparkles: Add: `get.cached.value` function to extract and divide logic of inquire cached value

---
+++ packages/utils/get.cached.value.ts
@@ -0,0 +1,23 @@
+import type { Cache } from "./cache"
+
+type CacheFindFallback<T> = () => T
+
+/**
+ * @param store caching store
+ * @param key store inquire key
+ * @param cacheFindFallback when cache is not found, caching it
+ * @returns cached value
+ */
+const getCachedValue = <CacheValue>(
+    store: Cache<CacheValue>,
+    key: string,
+    cacheFindFallback: CacheFindFallback<CacheValue>
+): CacheValue => {
+    if (store.has(key)) return store.get(key) as CacheValue
+
+    const newCacheValue = cacheFindFallback()
+    store.set(key, newCacheValue)
+    return newCacheValue
+}
+
+export { getCachedValue }
