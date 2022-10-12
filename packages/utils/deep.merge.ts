e36654a654dbd71e195dab462ccc5d23dde0bfe9
Danpa_cho <danpa725@cau.ac.kr>
2022-10-12 22:33:33 +0900

    :label: Update: `deep.merge` argument type with generic

--- packages/utils/deep.merge.ts
+++ packages/utils/deep.merge.ts
@@ -1,32 +1,30 @@
+/* eslint-disable @typescript-eslint/no-extra-semi */
 import type { NestedObject } from "./nested.object.type"
 
 /**
  * @param object string key object
  * @returns Merge nested style object deeply
  */
-const deepMerge = <T extends NestedObject>(...obj: T[]): NestedObject => {
-    const mergedObject: NestedObject = obj.reduce<NestedObject>(
-        (merged, obj) => {
-            const objectEntries = Object.entries<unknown>(obj)
-            for (const [key, value] of objectEntries) {
-                if (merged[key] === undefined) {
-                    merged[key] = value
+const deepMerge = <T extends NestedObject>(...obj: T[]): T => {
+    const mergedObject: T = obj.reduce<T>((merged, obj) => {
+        const objectEntries = Object.entries<unknown>(obj)
+        for (const [key, value] of objectEntries) {
+            if (merged[key] === undefined) {
+                ;(merged as NestedObject)[key] = value
+            } else {
+                if (typeof value === "string") {
+                    ;(merged as NestedObject)[key] = value
                 } else {
-                    if (typeof value === "string") {
-                        merged[key] = value
-                    } else {
-                        const res = deepMerge(
-                            merged[key] as NestedObject,
-                            value as NestedObject
-                        )
-                        merged[key] = res
-                    }
+                    const res = deepMerge(
+                        merged[key] as NestedObject,
+                        value as NestedObject
+                    )
+                    ;(merged as NestedObject)[key] = res
                 }
             }
-            return merged
-        },
-        {} as NestedObject
-    )
+        }
+        return merged
+    }, {} as T)
 
     return mergedObject
 }
