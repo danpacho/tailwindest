1e1d6c48680c523dfb86a38dcee14eda9433f931
Danpa_cho <danpa725@cau.ac.kr>
2022-10-12 22:57:20 +0900

    :sparkles: Update: Distinguishing how to use wind api and made a new api design according to the concept
    Dividing into basic style `wind` and variant style `$wind` and simplify cache inquiring logic with `get.cached.value` function and update document

--- packages/index.ts
+++ packages/index.ts
@@ -1,11 +1,11 @@
 import { Tailwindest as TailwindestType } from "./types/tailwindest"
-import { deepMerge, getTwClass } from "./utils"
-import { windestCache } from "./windest.cache"
+import { cache, deepMerge, getCachedValue, getTailwindClass } from "./utils"
 
 type StringKey<Key> = Extract<Key, string>
-type VariantsObject = {
-    [key: string]: TailwindestType
-}
+type VariantStyles<Key extends string, T> = Record<Key, T>
+
+/** @note cache key for base `style` and `class` */
+const BASE_KEY = "_" as const
 
 /**
  * @param baseStyle base tailwind style
@@ -13,87 +13,186 @@ type VariantsObject = {
  * @note **variant** is key of `variantsStyles`
  * @returns `class` tailwind class extractor **function**
  * @returns `style` input style extractor **function**, use it for composing styles
+ * @returns `compose` composed style of wind style set
  */
-function wind<VariantsStyle extends VariantsObject>(
-    baseStyle: TailwindestType,
-    VariantsStyles: VariantsStyle
+function windCore<
+    T extends TailwindestType,
+    Styles extends VariantStyles<string, T>
+>(
+    baseStyle: T,
+    variantsStyles: Styles
 ): {
-    class: (variant?: StringKey<keyof VariantsStyle>) => string
-    style: (variant?: StringKey<keyof VariantsStyle>) => TailwindestType
+    /**
+     * @note class extractor `function`
+     * @example
+     * const boxClass = wind$("variant")({ ...yourStyles 💄 }).class("variant")
+     * // 📜 returns yourStyles 💄 class string
+     */
+    class: (variant?: StringKey<keyof Styles>) => string
+    /**
+     * @note input style extractor `function`, use it to compose styles
+     * @example
+     * const boxStyle = wind$("variant")({ ...yourStyles 💄 }).style("variant")
+     * // 📜 returns { ...yourStyles 💄 }
+     */
+    style: (variant?: StringKey<keyof Styles>) => T
+    /**
+     * @note `compose` get multiple styles and compose to one object
+     * @example
+     * const boxComposed = wind$("variant")({ ...yourStyles 💄 }).compose(myStyle 🎁)
+     * // 📜 returns composed result of yourStyles 💄 & myStyle 🎁
+     */
+    compose: (...styles: T[]) => ReturnType<typeof windCore<T, Styles>>
 }
-function wind(baseStyle: TailwindestType): {
+function windCore<T extends TailwindestType>(
+    baseStyle: T
+): {
+    /**
+     * @note class extractor `function`
+     * @example
+     * const boxClass = wind({ ...yourStyles 💄 }).class()
+     * // 📜 returns yourStyles 💄 class string
+     */
     class: () => string
-    style: () => TailwindestType
+    /**
+     * @note input style extractor `function`, use it to compose styles
+     * @example
+     * const boxStyle = wind({ ...yourStyles 💄 }).style()
+     * // 📜 returns { ...yourStyles 💄 }
+     */
+    style: () => T
+    /**
+     * @note `compose` get multiple styles and compose to one object
+     * @example
+     * const boxComposed = wind({ ...yourStyles 💄 }).compose(myStyle 🎁)
+     * // 📜 returns composed result of yourStyles 💄 & myStyle 🎁
+     */
+    compose: (...styles: T[]) => ReturnType<typeof windCore<T>>
 }
+function windCore<
+    T extends TailwindestType,
+    Styles extends VariantStyles<string, T>
+>(baseStyle: T, variantsStyles?: Styles) {
+    const classStore = cache<string>()
+    const styleStore = cache<T>()
 
-function wind<VariantsStyle extends VariantsObject>(
-    baseStyle: TailwindestType,
-    variantsStyles?: VariantsStyle
-) {
-    const { getClass, setClass, hasClass, getStyle, setStyle, hasStyle } =
-        windestCache()
-
-    const baseClass = getTwClass(baseStyle)
+    classStore.set(BASE_KEY, getTailwindClass(baseStyle))
 
     return {
-        class: (variant?: StringKey<keyof VariantsStyle>) => {
+        class: (variant?: StringKey<keyof Styles>) => {
+            const cachedBaseStyle = getCachedValue<T>(
+                styleStore,
+                BASE_KEY,
+                () => baseStyle
+            )
+            const cachedBaseClass = getCachedValue<string>(
+                classStore,
+                BASE_KEY,
+                () => getTailwindClass(cachedBaseStyle)
+            )
+
             const isBase = variantsStyles === undefined || variant === undefined
-            if (isBase) return baseClass
+            if (isBase) return cachedBaseClass
 
-            const shouldCacheClass = hasClass(variant) === false
-            if (shouldCacheClass) {
-                const shouldCacheStyle = hasStyle(variant) === false
-                if (shouldCacheStyle) {
-                    const variantStyle: TailwindestType = deepMerge(
-                        baseStyle,
-                        variantsStyles[variant] as TailwindestType
-                    )
+            const cachedVariantStyle = getCachedValue<T>(
+                styleStore,
+                variant,
+                () =>
+                    deepMerge<T>(cachedBaseStyle, variantsStyles[variant] as T)
+            )
+            const cachedVariantClass = getCachedValue<string>(
+                classStore,
+                variant,
+                () => getTailwindClass(cachedVariantStyle)
+            )
 
-                    const variantClass = getTwClass(variantStyle)
-                    setStyle(variant, variantStyle)
-                    setClass(variant, variantClass)
-                    return variantClass
-                }
-                const cachedStyle = getStyle(variant) as TailwindestType
-                const variantClass = getTwClass(cachedStyle)
-                setClass(variant, variantClass)
-                return variantClass
-            }
-
-            const cachedClass = getClass(variant) as string
-            return cachedClass
+            return cachedVariantClass
         },
 
-        style: (variant?: StringKey<keyof VariantsStyle>): TailwindestType => {
+        style: (variant?: StringKey<keyof Styles>): T => {
+            const cachedBaseStyle = getCachedValue<T>(
+                styleStore,
+                BASE_KEY,
+                () => baseStyle
+            )
+
             const isBase = variantsStyles === undefined || variant === undefined
-            if (isBase) return baseStyle
+            if (isBase) return cachedBaseStyle
 
-            const shouldCacheStyle = hasStyle(variant) === false
-            if (shouldCacheStyle && variantsStyles[variant]) {
-                const variantStyle: TailwindestType = deepMerge(
-                    baseStyle,
-                    variantsStyles[variant] as TailwindestType
-                )
-                setStyle(variant, variantStyle)
-                return variantStyle
-            }
+            const cachedVariantStyle = getCachedValue<T>(
+                styleStore,
+                variant,
+                () =>
+                    deepMerge<T>(cachedBaseStyle, variantsStyles[variant] as T)
+            )
+            return cachedVariantStyle
+        },
 
-            const cachedStyle = getStyle(variant) as TailwindestType
-            return cachedStyle
+        compose: function (
+            ...styles: T[]
+        ): ReturnType<typeof windCore<T, Styles>> {
+            const cachedBaseStyle = styleStore.has(BASE_KEY)
+                ? (styleStore.get(BASE_KEY) as T)
+                : baseStyle
+
+            const composedStyle = styles.reduce<T>(
+                (composedStyle, currStyle) =>
+                    deepMerge<T>(composedStyle, currStyle),
+                cachedBaseStyle
+            )
+            const composedClass = getTailwindClass(composedStyle)
+            classStore.set(BASE_KEY, composedClass)
+            styleStore.set(BASE_KEY, composedStyle)
+
+            return this
         },
     }
 }
 
 /**
- * @param styles
- * @returns composed style of wind style set
+ * @note createWind with `custom` type
+ * @note gets generic extends `Tailwindest` type
+ * @returns `wind$` - get `variants` input, should not `_`
+ * @returns `wind` - just wind function
  */
-function composeWind(...styles: TailwindestType[]): TailwindestType {
-    return styles.reduce<TailwindestType>(
-        (style, curr) => deepMerge(style, curr),
-        {} as TailwindestType
-    )
+function createWind<T extends TailwindestType>(): {
+    wind$: <Variant extends string>(
+        ...variants: Variant[]
+    ) => typeof windCore<T, VariantStyles<Variant, T>>
+    wind: typeof windCore<T>
 }
+function createWind() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const wind$ = (...variants: string[]) => windCore
+    return {
+        wind$,
+        wind: windCore,
+    }
+}
+
+const defaultWind = createWind()
+/**
+ * @param baseStyle base tailwind style
+ * @returns `class` tailwind class extractor **function**
+ * @returns `style` input style extractor **function**, use it for composing styles
+ */
+const wind: (
+    baseStyle: TailwindestType
+) => ReturnType<typeof windCore<TailwindestType>> = defaultWind.wind
+
+/**
+ * @param ...variants tailwind variant names
+ * @note `variants` should not be `_`, it is default key
+ * @returns `wind$(...variants)` tailwind style generator like `wind`
+ */
+const wind$: <Variant extends string>(
+    ...variants: Variant[]
+) => (
+    baseStyle: TailwindestType,
+    variantsStyles: VariantStyles<Variant, TailwindestType>
+) => ReturnType<
+    typeof windCore<TailwindestType, VariantStyles<Variant, TailwindestType>>
+> = defaultWind.wind$
 
 /**
  * Get variants type of `wind` style,
@@ -116,11 +215,11 @@ export type WindVariants<WindStyle> = WindStyle extends {
     : never
 
 /**
- * Taiwindest total type set
+ * @note `Tailwindest` total type set
  * @example
  * type FontSize = Tailwindest["fontSize"]
- * // Get all type of tailwind fontSize
+ * // 📜 Get all type of tailwind fontSize
  */
 export type Tailwindest = TailwindestType
 
-export { wind, composeWind }
+export { createWind, wind, wind$ }
