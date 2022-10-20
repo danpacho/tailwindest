5ad4d827aec7730b7c4c3fa7f65b926659b0cf70
Danpa_cho <danpa725@cau.ac.kr>
2022-10-20 13:54:10 +0900

    :label: Update: `tailwind.sizing` pluggable type

--- packages/types/tailwind.sizing/index.ts
+++ packages/types/tailwind.sizing/index.ts
@@ -9,10 +9,26 @@ import {
     TailwindWidthType,
 } from "./@width"
 
-export interface TailwindSizing
-    extends TailwindHeightType,
-        TailwindMaxHeightType,
-        TailwindMinHeightType,
-        TailwindMaxWidthType,
-        TailwindMinWidthType,
-        TailwindWidthType {}
+export interface TailwindSizing<
+    TailwindSpacing extends string,
+    SizingPlug extends {
+        width?: string
+        maxWidth?: string
+        minWidth?: string
+        height?: string
+        maxHeight?: string
+        minHeight?: string
+    } = {
+        width: ""
+        maxWidth: ""
+        minWidth: ""
+        height: ""
+        maxHeight: ""
+        minHeight: ""
+    }
+> extends TailwindMinHeightType<SizingPlug["minHeight"]>,
+        TailwindHeightType<TailwindSpacing, SizingPlug["height"]>,
+        TailwindMaxHeightType<TailwindSpacing, SizingPlug["maxHeight"]>,
+        TailwindMinWidthType<SizingPlug["minWidth"]>,
+        TailwindMaxWidthType<SizingPlug["maxHeight"]>,
+        TailwindWidthType<TailwindSpacing, SizingPlug["width"]> {}
