93afa0f953d81c45b80263c977c3325c6fe68a0b
Danpa_cho <danpa725@cau.ac.kr>
2022-10-22 15:13:19 +0900

    :label: Add: `nest.plugin` plugin option type

--- packages/types/tailwind.plugin.option.ts
+++ packages/types/tailwind.plugin.option.ts
@@ -1,14 +1,25 @@
-import { PluginOption } from "./plugin"
-import { TailwindColorPluginOptionKey } from "./tailwind.common/@color"
-import { TailwindStylePluginKey } from "./tailwind.plugin.key"
+import type { PluginOption } from "./plugin"
+import type { TailwindColorPluginOptionKey } from "./tailwind.common/@color"
+import type {
+    TailwindNestPluginKey,
+    TailwindStylePluginKey,
+} from "./tailwind.plugin.key"
+
+export type TailwindStylePlugOption = PluginOption<TailwindStylePluginKey>
+export type TailwindDefaultStylePlug = PluginOption<TailwindStylePluginKey, "">
+
+export type TailwindestNestPlugOption = PluginOption<TailwindNestPluginKey>
+export type TailwindestDefaultNestPlug = PluginOption<TailwindNestPluginKey, "">
 
 type TailwindGlobalPlugOptionKey = TailwindColorPluginOptionKey | "sizing"
-export type TailwindGlobalPlugOption = PluginOption<TailwindGlobalPlugOptionKey>
+export type TailwindGlobalPlugOption =
+    PluginOption<TailwindGlobalPlugOptionKey> & {
+        screens?: TailwindestNestPlugOption
+    }
 
 export type TailwindDefaultGlobalPlugOption = PluginOption<
     TailwindGlobalPlugOptionKey,
     ""
->
-
-export type TailwindStylePlugOption = PluginOption<TailwindStylePluginKey>
-export type TailwindDefaultStylePlug = PluginOption<TailwindStylePluginKey, "">
+> & {
+    screens?: TailwindestDefaultNestPlug
+}
