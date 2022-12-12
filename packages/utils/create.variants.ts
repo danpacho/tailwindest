2f2a4ad079fe9a4f6746671949d0dafe645768f8
Danpa_cho <danpa725@cau.ac.kr>
2022-12-12 23:38:16 +0900

    :recycle: refacotor: remove `getCachedValue`

--- packages/utils/create.variants.ts
+++ packages/utils/create.variants.ts
@@ -1,4 +1,4 @@
-import { cache, deepMerge, getCachedValue, getTailwindClass } from "../core"
+import { cache, deepMerge, getTailwindClass } from "../core"
 import type { NestedObject } from "../core/nested.object.type"
 import type { WindVariants } from "../wind.variants"
 
@@ -37,24 +37,20 @@ type VariantsKeys<T extends VariantsStyle> = {
 const createVariants = <T extends VariantsStyle>(variantsStyle: T) => {
     const variantCache = cache<string, string>()
     return (variantsOption: VariantsKeys<T>): string =>
-        getCachedValue(
-            variantCache,
-            Object.values(variantsOption).join(""),
-            () =>
-                getTailwindClass(
-                    Object.entries(variantsStyle)
-                        .map(([variantsKey, variantsWind]) => {
-                            const option = variantsOption[variantsKey]
-                            return option
-                                ? variantsWind.style(option)
-                                : variantsWind.style()
-                        })
-                        .reduce<NestedObject>(
-                            (accStyle, currStyle) =>
-                                deepMerge(accStyle, currStyle),
-                            {}
-                        )
-                )
+        variantCache.get(Object.values(variantsOption).join(""), () =>
+            getTailwindClass(
+                Object.entries(variantsStyle)
+                    .map(([variantsKey, variantsWind]) => {
+                        const option = variantsOption[variantsKey]
+                        return option
+                            ? variantsWind.style(option)
+                            : variantsWind.style()
+                    })
+                    .reduce<NestedObject>(
+                        (accStyle, currStyle) => deepMerge(accStyle, currStyle),
+                        {}
+                    )
+            )
         )
 }
 
