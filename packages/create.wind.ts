ef9360b33903ea59c5abb8372a3178f3fe0ef01f
Danpa_cho <danpa725@cau.ac.kr>
2023-04-09 17:06:39 +0900

    :memo: docs: update `createWind` docs

--- packages/create.wind.ts
+++ packages/create.wind.ts
@@ -3,14 +3,14 @@ import { mergeProps, toggle } from "./utils"
 import { VariantsStyles, wind as windCore } from "./wind"
 
 /**
- * @description Create `wind`, `wind$` with customized `Tailwindest`
+ * @description Create tools with `Tailwindest` type
  * @example
  * // ✅ Add "my-color1" | "my-color2"
  * type Custom = Tailwindest<{
  *     color: "my-color1" | "my-color2",
  * }>
  *
- * // ✅ Plug custom type in generic
+ * // ✅ Plug custom type in generic, get all tools
  * const { wind, wind$, mergeProps, toggle } = createWind<Custom>()
  */
 const createWind = <StyleType extends NestedObject>() => {
@@ -25,9 +25,88 @@ const createWind = <StyleType extends NestedObject>() => {
     const toggleWithType = toggle(wind)
 
     return {
-        wind$,
+        /**
+         * @description Create `tailwind` style
+         * @example
+         * // ✅ Create complex style with wind
+         * const box = wind(
+         *   { ...boxStyle },
+         * )
+         * // 📸 Consolidate as [string]
+         * const boxClass = box.class()
+         *
+         * // 📸 Consolidate as styleSheet [object]
+         * const boxClass = box.style()
+         */
         wind,
+        /**
+         * @description Create `tailwind` style with variants
+         * @example
+         * // ✅ Create complex variant styles with wind$
+         * const button = wind$("success", "fail")(
+         *   { ...buttonBaseStyle },
+         *   {
+         *       // ✅ Define variants style
+         *       success: { ...successStyle },
+         *       fail: { ...failStyle },
+         *   }
+         * )
+         * // ✅ Get base style without argument
+         * const buttonBaseClass = button.class()
+         *
+         * // ✅ Get specific variants with argument
+         * const buttonSuccessClass = button.class("success")
+         */
+        wind$,
+        /**
+         * @description merge two styles
+         * @example
+         * ```tsx
+         * // ✅ Add specific style props
+         * const Text = ({
+         *      children,
+         *      borderColor,
+         * }: {
+         *      children: ReactNode
+         *      borderColor: Tailwindest["borderColor"]
+         * }) => {
+         *      const { children, ...option } = props
+         *      return (
+         *         <p
+         *             className={mergeProps(
+         *                 {
+         *                     // text styleSheet
+         *                 },
+         *                 {
+         *                     // add borderColor as component props
+         *                     borderColor,
+         *                 }
+         *             )}
+         *         >
+         *             {children}
+         *         </p>
+         *     )
+         * }
+         * ```
+         */
         mergeProps: mergePropsWithType,
+        /**
+         * @description Toggling style by `boolean` condition
+         * @example
+         * // 💡 Boolean condition
+         * const isClicked = true;
+         *
+         * // ✅ Toggle between two styles
+         * const className = toggle(isClicked, {
+         *      truthy: {
+         *          backgroundColor: "bg-green-500",
+         *      },
+         *      falsy: {
+         *          backgroundColor: "bg-red-500",
+         *      },
+         * })
+         * // bg-green-500 (truthy)
+         */
         toggle: toggleWithType,
     }
 }
