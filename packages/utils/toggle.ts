51d18dd15222c401911bee7ab0ad471ab5bd0c7b
Danpa_cho <danpa725@cau.ac.kr>
2023-04-09 17:04:12 +0900

    :memo: feat: add `tsdoc` for `toggle` arguments

--- packages/utils/toggle.ts
+++ packages/utils/toggle.ts
@@ -3,20 +3,29 @@ import type { NestedObject } from "../core/nested.object.type"
 import { wind } from "../wind"
 
 type Toggle<StyleType extends NestedObject> = {
+    /**
+     * `true` condition `styleSheet`
+     */
     truthy: StyleType
+    /**
+     * `false` condition `styleSheet`
+     */
     falsy: StyleType
+    /**
+     * base `styleSheet`, `[ optional ]`
+     */
     base?: StyleType
 }
 
 const toggle =
     <Wind extends typeof wind>(wind: Wind) =>
     (
-        condition: boolean,
+        booleanCondition: boolean,
         { falsy, truthy, base }: Toggle<Parameters<Wind>[0]>
     ) =>
         wind(base ?? {}, {
             f: falsy,
             t: truthy,
-        }).class(condition ? "t" : "f")
+        }).class(booleanCondition ? "t" : "f")
 
 export { toggle }
