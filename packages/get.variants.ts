0f06a110158e3553525bc0da71fef88b296766a7
Danpa_cho <danpa725@cau.ac.kr>
2023-04-25 11:46:16 +0900

    :recycle: refactor: change name to `GetVariants` and improve type inference performance

---
+++ packages/get.variants.ts
@@ -0,0 +1,48 @@
+/**
+ * @description Get variants type of `rotary` and `variants`
+ * @example
+ * ```ts
+ * // ✅ rotary
+ * const rotaryBtn = tw.rotary({
+ *      success: {},
+ *      fail: {},
+ * })
+ * type InferredRotary = GetVariants<typeof rotaryBtn>
+ * type InferredRotary = "success" | "fail"
+ *
+ * // ✅ variants
+ * const complexBtn = tw.variants({
+ *      size: {
+ *          sm: {},
+ *          md: {},
+ *          lg: {},
+ *      },
+ *      fail: {
+ *          success: {},
+ *          fail: {},
+ *      }
+ * })
+ * type InferredVariants = GetVariants<typeof complexBtn>
+ * type InferredVariants = {
+ *      size?: "sm" | "md" | "lg"
+ *      type?: "success" | "fail"
+ * }
+ *
+ * // ❌ never
+ * const never = tw.toggle({
+ *      truthy: {},
+ *      falsy: {}
+ * })
+ * type InferredToggle = GetVariants<typeof never>
+ * type InferredToggle = never
+ * ```
+ */
+export type GetVariants<TypeofVariants> = TypeofVariants extends {
+    class: (variants: infer Variants) => unknown
+}
+    ? Variants extends string
+        ? Variants
+        : never
+    : TypeofVariants extends (VariantsOption: infer VariantOptions) => unknown
+    ? Partial<VariantOptions>
+    : never
