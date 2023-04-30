fe186140341c42bf9e3057d625702d9d252ab8eb
Danpa_cho <danpa725@cau.ac.kr>
2023-04-30 12:36:12 +0900

    :sparkles: feat: update the `variants` API to be the closed to `stitches` and improve performance

--- packages/core/tools/create.variants.ts
+++ packages/core/tools/create.variants.ts
@@ -1,63 +1,87 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import type { NestedObject, ToString } from "../../utils"
+import { BASE_KEY } from "../../constants"
+import type { CacheKey, NestedObject, StyleGeneratorCache } from "../../utils"
 import { cache } from "../cache"
 import { deepMerge } from "../deep.merge"
 import { getTailwindClass } from "../get.tailwind.class"
-import { createRotary } from "./create.rotary"
-import type { StyleGeneratorRotary } from "./tool.interface"
+import { StyleGeneratorVariants } from "./tool.interface"
 
-const createVariants = <StyleType extends NestedObject>() => {
-    const rotary = createRotary<StyleType>()
-    const variantCache = cache<string, string>()
-    const rotaryCache = new Map<
-        string,
-        StyleGeneratorRotary<StyleType, string>
-    >()
-
-    return <VariantsList>(variantsStyle: {
-        [RotaryVariants in keyof VariantsList]: Record<
-            keyof VariantsList[RotaryVariants],
-            StyleType
-        > & {
-            base?: StyleType
-        }
-    }) => {
-        Object.entries<{
-            [key in string]: StyleType
-        }>(variantsStyle).forEach(([variant, rotaryStyles]) => {
-            rotaryCache.set(variant, rotary(rotaryStyles))
-        })
-
-        return (variantsOption: {
-            [Key in keyof VariantsList]: Exclude<
-                ToString<keyof VariantsList[Key]>,
-                "base"
+const createVariants =
+    <StyleType extends NestedObject>() =>
+    <VariantsList>({
+        base,
+        variants,
+    }: {
+        variants: {
+            [RotaryVariants in keyof VariantsList]: Record<
+                keyof VariantsList[RotaryVariants],
+                StyleType
             >
-        }): string => {
-            const variantKeys = Object.keys(variantsOption)
-            const variantValues = Object.values(variantsOption)
+        }
+    } & {
+        base?: StyleType
+    }): StyleGeneratorVariants<
+        StyleType,
+        {
+            [Key in keyof VariantsList]: keyof VariantsList[Key]
+        }
+    > => {
+        const variantCache = cache<CacheKey, StyleGeneratorCache<StyleType>>()
+        let isBaseUpdated = false
 
-            return variantCache.get(variantValues.join(""), () =>
-                getTailwindClass(
-                    variantKeys
-                        .map((variantOptionKey) =>
-                            rotaryCache
-                                .get(variantOptionKey)!
-                                .style(
-                                    variantsOption[
-                                        variantOptionKey as keyof VariantsList
-                                    ] satisfies string
-                                )
-                        )
-                        .reduce(
-                            (accStyle, currStyle) =>
-                                deepMerge(accStyle, currStyle),
-                            {}
-                        )
-                )
+        const getCachedBaseStyle = (): StyleType =>
+            variantCache.get(BASE_KEY, () => [base ?? ({} as StyleType), ""])[0]
+
+        const getUpdatedVariantCacheValue = (
+            variantOption: {
+                [Key in keyof VariantsList]: keyof VariantsList[Key]
+            },
+            baseStyle: StyleType
+        ): StyleGeneratorCache<StyleType> => {
+            const updatedVariantStyle = Object.keys(variantOption).reduce(
+                (accStyle, key) =>
+                    deepMerge(
+                        accStyle,
+                        variants[key as keyof VariantsList][
+                            variantOption[key as keyof VariantsList] // variant option
+                        ]
+                    ),
+                baseStyle
             )
+            return [updatedVariantStyle, getTailwindClass(updatedVariantStyle)]
+        }
+
+        const getCachedVariant = (variantOption: {
+            [Key in keyof VariantsList]: keyof VariantsList[Key]
+        }) => {
+            const cachedVariantKey = Object.values(variantOption).join("")
+
+            if (isBaseUpdated) {
+                const updatedCacheValue = getUpdatedVariantCacheValue(
+                    variantOption,
+                    getCachedBaseStyle()
+                )
+                variantCache.set(cachedVariantKey, updatedCacheValue)
+                isBaseUpdated = false
+                return updatedCacheValue
+            }
+
+            const cachedVariant = variantCache.get(cachedVariantKey, () =>
+                getUpdatedVariantCacheValue(variantOption, getCachedBaseStyle())
+            )
+            return cachedVariant
+        }
+
+        return {
+            style: (variantOption) => getCachedVariant(variantOption)[0],
+            class: (variantOption) => getCachedVariant(variantOption)[1],
+            compose: function (...styles) {
+                const composedStyle = deepMerge(getCachedBaseStyle(), ...styles)
+                variantCache.set(BASE_KEY, [composedStyle, ""])
+                isBaseUpdated = true
+                return this
+            },
         }
     }
-}
 
 export { createVariants }
