b6fec18605917bccf9b20bb04906ae092a65bd23
Danpa_cho <danpa725@cau.ac.kr>
2023-05-10 23:11:59 +0900

    :bug: fix: incorrect type definition of directional transform, `scaleX` and `scaleY`

--- packages/types/tailwind.transforms/@scale.ts
+++ packages/types/tailwind.transforms/@scale.ts
@@ -10,10 +10,10 @@ export type TailwindScaleType<Scale extends string> = {
      *@description Utilities for scaling elements with transform x direction.
      *@see {@link https://tailwindcss.com/docs/scale scale-x}
      */
-    transformScaleX: PluginVariantsWithDirection<"scale", Scale>
+    transformScaleX: PluginVariantsWithDirection<"scale-x", Scale>
     /**
      *@description Utilities for scaling elements with transform y direction.
      *@see {@link https://tailwindcss.com/docs/scale scale-y}
      */
-    transformScaleY: PluginVariantsWithDirection<"scale", Scale>
+    transformScaleY: PluginVariantsWithDirection<"scale-y", Scale>
 }
