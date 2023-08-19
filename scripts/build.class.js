957da3476c46a1c1b12a4e6457b3e2515e222a78
Danpa_cho <danpa725@cau.ac.kr>
2023-08-19 19:31:29 +0900

    :recycle: refactor: nest list generation script to divide category of nest conditions

--- scripts/build.class.js
+++ scripts/build.class.js
@@ -1,127 +1,92 @@
 import { writeFile } from "fs"
 
 const PSEUDO_ELEMENTS = [
-    "::before",
-    "::after",
-    "::placeholder",
-    "::file",
-    "::marker",
-    "::backdrop",
-    "::selection",
-    "::first-line",
-    "::first-letter",
+    "before",
+    "after",
+    "placeholder",
+    "file",
+    "marker",
+    "backdrop",
+    "selection",
+    "first-line",
+    "first-letter",
 ]
 const PSEUDO_CLASSES = [
-    ":hover",
-    ":active",
-    ":first",
-    ":last",
-    ":only",
-    ":odd",
-    ":even",
-    ":first-of-type",
-    ":last-of-type",
-    ":only-of-type",
-    ":empty",
-    ":enabled",
-    ":indeterminate",
-    ":default",
-    ":required",
-    ":valid",
-    ":invalid",
-    ":in-range",
-    ":out-of-range",
-    ":placeholder-shown",
-    ":autofill",
-    ":read-only",
-    ":checked",
-    ":disabled",
-    ":visited",
-    ":target",
-    ":focus",
-    ":focus-within",
-    ":focus-visible",
-    ":optional",
+    "hover",
+    "active",
+    "first",
+    "last",
+    "only",
+    "odd",
+    "even",
+    "first-of-type",
+    "last-of-type",
+    "only-of-type",
+    "empty",
+    "enabled",
+    "indeterminate",
+    "default",
+    "required",
+    "valid",
+    "invalid",
+    "in-range",
+    "out-of-range",
+    "placeholder-shown",
+    "autofill",
+    "read-only",
+    "checked",
+    "disabled",
+    "visited",
+    "target",
+    "focus",
+    "focus-within",
+    "focus-visible",
+    "optional",
 ]
 
 const MEDIA_CONDITIONS = [
-    "@contrast-more",
-    "@contrast-less",
-    "@motion-reduce",
-    "@motion-safe",
-    "@portrait",
-    "@landscape",
-    "@print",
-    "@rtl",
-    "@ltr",
+    "contrast-more",
+    "contrast-less",
+    "motion-reduce",
+    "motion-safe",
+    "portrait",
+    "landscape",
+    "print",
+    "rtl",
+    "ltr",
 ]
 
-const BREAK_CONDITIONS = [
-    "@sm",
-    "@md",
-    "@lg",
-    "@xl",
-    "@2xl",
-    "@max-sm",
-    "@max-md",
-    "@max-lg",
-    "@max-xl",
-    "@max-2xl",
+const SCREEN_CONDITIONS = [
+    "sm",
+    "md",
+    "lg",
+    "xl",
+    "2xl",
+    "max-sm",
+    "max-md",
+    "max-lg",
+    "max-xl",
+    "max-2xl",
 ]
 
 const ARIA_CONDITIONS = [
-    "@aria-checked",
-    "@aria-disabled",
-    "@aria-expanded",
-    "@aria-hidden",
-    "@aria-pressed",
-    "@aria-readonly",
-    "@aria-required",
-    "@aria-selected",
+    "aria-checked",
+    "aria-disabled",
+    "aria-expanded",
+    "aria-hidden",
+    "aria-pressed",
+    "aria-readonly",
+    "aria-required",
+    "aria-selected",
 ]
 
-const THEME_CONDITION = ["@dark"]
+const THEME_CONDITION = ["dark"]
 
-const BasicNestKeys = [
-    ...PSEUDO_CLASSES,
-    ...PSEUDO_ELEMENTS,
+const BREAK_CONDITIONS = [
     ...MEDIA_CONDITIONS,
-    ...BREAK_CONDITIONS,
+    ...SCREEN_CONDITIONS,
     ...ARIA_CONDITIONS,
     ...THEME_CONDITION,
-]
-
-const SHORT_HANDED_IDENTIFIER = "$"
-
-/**
- * Replaces `:`, `::`, and `@` with `$`.
- * @param {string[]} classNames - The input array of strings to transform.
- */
-const toShortTailwindestIdentifier = (classNames) =>
-    classNames.map((inputString) => {
-        // replace ':', '::', '@' with '$'
-        const replacedInputString = inputString
-            .replaceAll(/@/g, SHORT_HANDED_IDENTIFIER)
-            .replaceAll(/::/g, SHORT_HANDED_IDENTIFIER)
-            .replaceAll(/:/g, SHORT_HANDED_IDENTIFIER)
-
-        return replacedInputString
-    })
-
-/**
- * @type {{fileName: string; typeName: string; types: string[]}[]}
- */
-const buildData = [
-    {
-        fileName: "basic",
-        typeName: "TailwindNestedBasicType",
-        types: BasicNestKeys,
-    },
-    {
-        fileName: "basic.short",
-        typeName: "ShortTailwindNestedBasicType",
-        types: toShortTailwindestIdentifier(BasicNestKeys),
-    },
 ]
 
 /**
@@ -142,28 +107,48 @@ const extractFile = (fileName, data) => {
  * @param {string} typeName
  * @returns {string} unionType
  */
-const toUnionTypes = (keys, typeName) => {
+const buildUnionTypes = (keys, typeName) => {
     const keyLength = keys.length - 1
-    const toUnion = keys.map((key, index) => {
+    const unionList = keys.map((key, index) => {
         const isLast = index === keyLength
         if (isLast) return `"${key}"`
         return `"${key}" |`
     })
-    const unionType = toUnion.join(" ")
+    const unionType = unionList.join(" ")
     const exportType = `export type ${typeName} = ${unionType}`
     return exportType
 }
 
+/**
+ * @type {{fileName: string; typeName: string; types: string[]}[]}
+ */
+const buildData = [
+    {
+        fileName: "break",
+        typeName: "TailwindBreakConditions",
+        types: BREAK_CONDITIONS,
+    },
+    {
+        fileName: "pseudo.class",
+        typeName: "TailwindPseudoClassConditions",
+        types: PSEUDO_CLASSES,
+    },
+    {
+        fileName: "pseudo.element",
+        typeName: "TailwindPseudoElementConditions",
+        types: PSEUDO_ELEMENTS,
+    },
+]
+
 const buildNestClass = () => {
     const TYPE_BUILD_INFO = {
-        LOCATION: `${process.cwd()}/packages/tailwindest/src/types/tailwind.nested`,
+        LOCATION: `${process.cwd()}/packages/tailwindest/src/types/nest.keys`,
         FILE_FORMAT: "ts",
-        IDENTIFIER: "@",
     }
 
     buildData.forEach((data) => {
-        const fileName = `${TYPE_BUILD_INFO.LOCATION}/${TYPE_BUILD_INFO.IDENTIFIER}${data.fileName}.${TYPE_BUILD_INFO.FILE_FORMAT}`
-        extractFile(fileName, toUnionTypes(data.types, data.typeName))
+        const fileName = `${TYPE_BUILD_INFO.LOCATION}/${data.fileName}.${TYPE_BUILD_INFO.FILE_FORMAT}`
+        extractFile(fileName, buildUnionTypes(data.types, data.typeName))
     })
 }
 
