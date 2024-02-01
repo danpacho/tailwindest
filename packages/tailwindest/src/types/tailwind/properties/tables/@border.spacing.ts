45b3e662cef559fa6089470a0e0ade41f31a2114
Danpa_cho <danpa725@cau.ac.kr>
2024-02-01 17:33:32 +0900

    :sparkles: feat: add borderSpacing customization feature

--- packages/tailwindest/src/types/tailwind/properties/tables/@border.spacing.ts
+++ packages/tailwindest/src/types/tailwind/properties/tables/@border.spacing.ts
@@ -1,19 +1,19 @@
 import { PluginVariants } from "../../../plugin"
 
-export type TailwindBorderSpacingType<BorderSpacing extends string> = {
+export type TailwindBorderSpacingType<TailwindSpacing extends string> = {
     /**
      *@description Utilities for controlling the spacing between table borders.
      *@see {@link https://tailwindcss.com/docs/border-spacing border-spacing}
      */
-    borderSpacing: PluginVariants<"border-spacing", BorderSpacing>
+    borderSpacing: PluginVariants<"border-spacing", TailwindSpacing>
     /**
      *@description Utilities for controlling the spacing between table borders x direction.
      *@see {@link https://tailwindcss.com/docs/border-spacing border-spacing-x}
      */
-    borderSpacingX: PluginVariants<"border-spacing-x", BorderSpacing>
+    borderSpacingX: PluginVariants<"border-spacing-x", TailwindSpacing>
     /**
      *@description Utilities for controlling the spacing between table borders y direction.
      *@see {@link https://tailwindcss.com/docs/border-spacing border-spacing-y}
      */
-    borderSpacingY: PluginVariants<"border-spacing-y", BorderSpacing>
+    borderSpacingY: PluginVariants<"border-spacing-y", TailwindSpacing>
 }
