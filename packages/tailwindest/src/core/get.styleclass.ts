653765a7e77f305e30b131b25b15471496ab032a
Danpa_cho <danpa725@cau.ac.kr>
2025-02-12 14:49:42 +0900

    ♻️ refactor: use literal, instead of using constant

--- packages/tailwindest/src/core/get.styleclass.ts
+++ packages/tailwindest/src/core/get.styleclass.ts
@@ -1,9 +1,7 @@
-const DIVIDER = " "
-
 /**
  * @param stylesArray flattened styles, which is `string[]`
  * @returns Join array into `string`
  */
-const getStyleClass = (stylesArray: string[]) => stylesArray.join(DIVIDER)
+const getStyleClass = (stylesArray: string[]) => stylesArray.join(" ")
 
 export { getStyleClass }
