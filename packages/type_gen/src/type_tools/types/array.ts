d172970a6adbed3db38e7a99f75207209679d824
Danpa_cho <danpa725@cau.ac.kr>
2025-02-17 00:11:51 +0900

    feat: add array type generator

---
+++ packages/type_gen/src/type_tools/types/array.ts
@@ -0,0 +1,41 @@
+import { toGenericTypeString } from "./to.generic.type.string"
+import { type GenericOption, Type } from "./type"
+
+class ArrayType extends Type {
+    constructor(
+        private elementType: Type,
+        alias?: string,
+        generic?: GenericOption
+    ) {
+        super()
+        this.alias = alias
+        this.generic = generic
+    }
+    private array(genericLiterals?: Array<string>): string {
+        return `Array<${this.elementType.toTypeString(genericLiterals)}>`
+    }
+    toTypeString(genericLiterals?: Array<string>): string {
+        return toGenericTypeString({
+            typegen: this.array.bind(this),
+            alias: this.alias,
+            generic: this.generic,
+            genericLiterals,
+        })
+    }
+    collectAliases(genericLiterals?: Array<string>): string[] {
+        const childAliases = this.elementType.collectAliases(genericLiterals)
+        const prefix = this.getTypePrefix()
+        const selfAlias = this.alias
+            ? [`${prefix}${this.array(genericLiterals)};`]
+            : []
+        return [...childAliases, ...selfAlias]
+    }
+}
+
+export function array(
+    type: Type,
+    alias?: string,
+    generic?: GenericOption
+): Type {
+    return new ArrayType(type, alias, generic)
+}
