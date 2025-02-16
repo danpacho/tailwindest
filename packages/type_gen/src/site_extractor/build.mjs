c8654ed91f96340188ee77ef66b75ac6c633602c
Danpa_cho <danpa725@cau.ac.kr>
2025-02-17 00:28:33 +0900

    feat: add build script for preparing generation process

---
+++ packages/type_gen/src/site_extractor/build.mjs
@@ -0,0 +1,44 @@
+import { fileExists } from "./util/fs.mjs"
+import { main as execDocs } from "./docs/main.mjs"
+import { main as execType } from "./types/main.mjs"
+import { join } from "node:path"
+import { STORE_ROOT } from "./constant.mjs"
+
+/**
+ * @param {string} filename
+ * @param {boolean} forceUpdate
+ * @returns {Promise<boolean>}
+ */
+const shouldUpdate = async (filename, forceUpdate = false) => {
+    return !(await fileExists(filename)) || forceUpdate
+}
+
+/**
+ * @param {Array<{ type: "doc" | "type", filename: string, forceUpdate?: boolean }>} generateOptions
+ */
+async function prepareStore(generateOptions) {
+    for (const gen of generateOptions) {
+        if (await shouldUpdate(gen.filename, gen.forceUpdate)) {
+            console.log("START")
+            switch (gen.type) {
+                case "doc":
+                    execDocs(gen.filename)
+                case "type":
+                    execType(gen.filename)
+            }
+            console.log("FIN")
+        }
+    }
+}
+
+async function main() {
+    const DOC_STORE = join(STORE_ROOT, "docs.json")
+    const TYPE_STORE = join(STORE_ROOT, "types.d.ts")
+
+    await prepareStore([
+        { type: "doc", filename: DOC_STORE, forceUpdate: true },
+        { type: "type", filename: TYPE_STORE, forceUpdate: true },
+    ])
+}
+
+await main()
