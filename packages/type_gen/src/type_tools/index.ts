f6ca71b9cb786239212d8e55e9c9f3875e7a065a
Danpa_cho <danpa725@cau.ac.kr>
2025-02-21 20:20:11 +0900

    rename: generator into type_generator

--- packages/type_gen/src/type_tools/index.ts
+++ packages/type_gen/src/type_tools/index.ts
@@ -1,2 +1,2 @@
 export * from "./types"
-export * from "./generator"
+export * from "./type_generator"
--- packages/type_gen/src/type_tools/type_generator.test.ts
+++ packages/type_gen/src/type_tools/type_generator.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest"
-import * as t from "./"
+import * as t from "."
 
 function normalize(str: string): string {
     return str.replace(/\s+/g, " ").trim()
