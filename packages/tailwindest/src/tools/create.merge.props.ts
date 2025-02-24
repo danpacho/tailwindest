eac6526106106346b37e40a8a26203424f9e3a1a
Danpa_cho <danpa725@cau.ac.kr>
2025-02-24 21:14:44 +0900

    remove: v2 legacy core & tools

--- packages/tailwindest/src/tools/create.merge.props.ts
+++
@@ -1,8 +0,0 @@
-import { deepMerge, getTailwindClass } from "../core"
-
-const createMergeProps =
-    <StyleType>() =>
-    (baseStyle: StyleType, styleProps: StyleType) =>
-        getTailwindClass(deepMerge(baseStyle, styleProps))
-
-export { createMergeProps }
