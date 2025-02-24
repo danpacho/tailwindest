bf2ff46cb53cd5af532bbe3d6040c3fa60cf8cac
Danpa_cho <danpa725@cau.ac.kr>
2025-02-24 22:29:56 +0900

    remove: unused core modules

--- packages/tailwindest/src/core/get.styleclass.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @param stylesArray flattened styles, which is `string[]`
- * @returns Join array into `string`
- */
-const getStyleClass = (stylesArray: string[]) => stylesArray.join(" ")
-
-export { getStyleClass }
