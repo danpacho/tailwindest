48ad2789916aaab22fa52feccd3eb8e079c8f3a0
Danpa_cho <danpa725@cau.ac.kr>
2025-02-24 23:48:12 +0900

    chore: export legacy typesets

--- packages/tailwindest/src/index.ts
+++ packages/tailwindest/src/index.ts
@@ -1,4 +1,9 @@
 import { createTools, type GetVariants } from "./tools"
 import { CreateTailwindest } from "./create_tailwindest"
 
+// V2 + tailwindcss < 4.0
+export type { Tailwindest } from "./legacy/tailwindest"
+export type { ShortTailwindest } from "./legacy/tailwindest.short"
+
+// V3 + tailwindcss >= 4.0
 export { createTools, type GetVariants, type CreateTailwindest }
