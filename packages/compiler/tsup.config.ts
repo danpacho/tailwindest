6e6c500f8db8f03dc7eea6ee41ccbcdd4b858723
Danpa_cho <danpa725@cau.ac.kr>
2026-04-28 21:09:03 +0900

    chore(compiler): update package metadata and e2e dependency surfaces

--- packages/compiler/tsup.config.ts
+++ packages/compiler/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup"
 export default defineConfig({
     entry: ["src/index.ts", "src/vite/index.ts"],
     format: ["esm"],
+    external: ["typescript"],
     outDir: "dist",
     outExtension: () => ({ js: ".mjs" }),
     dts: true,
